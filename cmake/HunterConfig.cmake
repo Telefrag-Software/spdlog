3cade26934e8e84245b438f675458ccfea4a3249
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-11-02 01:04:54 +0300

    spdlog Hunterization

---
+++ cmake/HunterConfig.cmake
@@ -0,0 +1,7 @@
+include_guard()
+
+hunter_config(fmt
+	URL "https://github.com/Telefrag-Software/fmt/archive/refs/heads/hunter-8.0.1.zip"
+	VERSION "8.0.1"
+	SHA1 "b5cd7cca9bda1490d76843dad25f9c585b37a5cb"
+)
